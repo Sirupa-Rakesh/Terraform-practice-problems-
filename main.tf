fc2efc396206ac6984693285571a48e135385bef
Sirupa-Rakesh <siruparakeshrakhi@gmail.com>
2026-03-27 18:09:23 +0530

    actions

--- main.tf
+++ main.tf
@@ -7,3 +7,18 @@ resource "aws_instance" "myserver" {
     ami = "ami-0220d79f3f480ecf5"
     instance_type = "t3.micro"
 }
+
+resource "security_group_id" "new terraform" {
+    ingress{
+        from_port = 22
+        to_port = 22
+        protocol = "tcp"
+    }
+  
+   ingress{
+    for_port = 80
+    to_port = 80
+    protocol = "http"
+    
+   }
+}
